b7bae1bea422a62533add20e7d60c9ae0f6690a3
crux.wild <jimubox.crux.wild@gmail.com>
2016-08-07 22:23:46 +0800

    bugfix

--- mvn-compile.js
+++ mvn-compile.js
@@ -2,16 +2,14 @@
 
 const shell = require('shelljs');
 
+process.env.FORCE_COLOR = 'true';
+
 if (!shell.which('java')) {
   process.stderr.write('please install [java] first.');
 }
 
 if (!shell.which('mvn')) {
   process.stderr.write('please install [mvn] first.');
-}
-
-if (shell.exec(`export FORCE_COLOR='true'`).code !== 0) {
-  process.stderr.write('export FORCE_COLOR >> execution failed.');
 }
 
 if (shell.exec('mvn clean').code !== 0) {
