6956d5fe5abb4c3a83d1978efddb5dce785f6853
crux.wild <jimubox.crux.wild@gmail.com>
2016-08-07 21:30:41 +0800

    bugfix

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ const path = require('path');
 
 class MavenCompilePlugin {
   constructor({ cwd = '' } = {}) {
-    this[Symbol.for('pluginName')] = 'mvn-compile';
     this[Symbol.for('initProcess')]();
   }
 
   [Symbol.for('initProcess')]() {
     const moduleDir = __dirname;
-    const scriptLocation = path.join(moduleDir, 'mvn-compile.js');
 
-    this[Symbol.for('process')] = spawn('node', [scriptLocation]);
+    const scriptPath = path.join(moduleDir, 'mvn-compile.js');
+
+    this[Symbol.for('process')] = spawn('node', [scriptPath]);
   }
 
   getProcess() {
     return this[Symbol.for('process')];
-  }
-
-  getPluginName() {
-    return this[Symbol.for('pluginName')];
   }
 };
 
