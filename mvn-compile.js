b65370a1db91b07e89afb23b4a3c4f58fc062422
jieling.guan <jieling.guan@jimu.com>
2016-08-11 12:15:47 +0800

    add

--- mvn-compile.js
+++ mvn-compile.js
@@ -1,23 +1,33 @@
 'use strict';
 
+const utils = require('./lib/utils');
+
 const shell = require('shelljs');
 
 const chalk = require('chalk');
 
-process.env.FORCE_COLOR = 'true';
+const co = require('co');
 
-if (!shell.which('java')) {
-  process.stderr.write('please install [java] first.');
-}
+co(function *() {
+  process.env.FORCE_COLOR = 'true';
 
-if (!shell.which('mvn')) {
-  process.stderr.write('please install [mvn] first.');
-}
+  if (!shell.which('java')) {
+    process.stderr.write('please install [java] first.');
+  }
 
-if (shell.exec('mvn clean').code !== 0) {
-  process.stderr.write('mvn clean >> execution failed.');
-}
+  if (!shell.which('mvn')) {
+    process.stderr.write('please install [mvn] first.');
+  }
 
-if (shell.exec('mvn compile').code !== 0) {
-  process.stderr.write('mvn compile >> execution failed.');
-}
+  let clean = yield utils.mvn('mvn', ['clean']);
+
+  if (clean !== 0) {
+    process.stderr.write('mvn clean >> execution failed.');
+  }
+
+  let compile = yield utils.mvn('mvn', ['compile']);
+
+  if (compile !== 0) {
+    process.stderr.write('mvn compile >> execution failed.');
+  }
+});
